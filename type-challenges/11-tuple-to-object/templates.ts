b72b64930e94039fb3c9f5f258403a0200cf4831
Huangyunze <973113137@qq.com>
2022-03-03 01:46:51 +0800

    add 11-tuple annotation

--- type-challenges/11-tuple-to-object/templates.ts
+++ type-challenges/11-tuple-to-object/templates.ts
@@ -1,2 +1,12 @@
 // 你的答案
-type TupleToObject<T extends readonly PropertyKey[]> =  { [K in T[number]]: K }
+type TupleToObject<T extends readonly PropertyKey[]> = { [K in T[number]]: K }
+
+
+/**
+ * T[number] 实际上会拿到数组值的一个联合类型，比如数组['h', 'y', 'z']，拿到的就是 'h' | 'y' | 'z'
+ */
+type arr = ['hyz', 'hdz', 'hhh'];
+type arrs<T extends any[]> = T[number]
+type c = arrs<arr> // 'hyz' | 'hdz' | 'hhh'
+
+/************************************************** */
